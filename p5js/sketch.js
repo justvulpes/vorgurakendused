f58fc420ddc7d971e31f613d3776f3d5da4e8464
rareba <rrebase@gmail.com>
2017-10-05 11:25:18 +0300

    update

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -1,10 +1,31 @@
+// for red, green, and blue color values
+let r, g, b;
+
 function setup() {
-    createCanvas(displayWidth, displayHeight);
-    strokeWeight(10);
-    stroke(0);
+    createCanvas(720, 400);
+    // Pick colors randomly
+    r = random(255);
+    g = random(255);
+    b = random(255);
 }
 
-function touchMoved() {
-    line(mouseX, mouseY, pmouseX, pmouseY);
-    return false;
+function draw() {
+    background(127);
+    // Draw a circle
+    strokeWeight(2);
+    stroke(r, g, b);
+    fill(r, g, b, 127);
+    ellipse(360, 200, 200, 200);
+}
+
+// When the user clicks the mouse
+function mousePressed() {
+    // Check if mouse is inside the circle
+    let d = dist(mouseX, mouseY, 360, 200);
+    if (d < 100) {
+        // Pick new random color values
+        r = random(255);
+        g = random(255);
+        b = random(255);
+    }
 }
