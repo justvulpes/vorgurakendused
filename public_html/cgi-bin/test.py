e000e9b341fa91456f06301152231f402eed600d
Ragnar Rebase <rareba@ttu.ee>
2017-10-27 21:47:07 +0300

    added results page, saving games-work in progress

--- public_html/cgi-bin/test.py
+++ public_html/cgi-bin/test.py
@@ -1,31 +1,126 @@
 #!/usr/bin/python3
 import cgi
 import cgitb
+import json
+import os
 import random
+
 cgitb.enable()
+results_path = "../prax3/results.json"
+save_path = "../prax3/saves.json"
 
 print("Content-type: text/html")
 print()
-print("""<style>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
 
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-}
 
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
-</style>""")
-r = "<table>"
-for i in range(1, 101):
-    msg = " has won" if random.getrandbits(1) else " has lost"
-    r += "<tr><td>Player " + str(i) + msg +"</td></tr>"
-r += "</table>"
-print("<html><head><title>test.py</title></head><body><h1>Minesweeper!</h1><p>Game results:</p>" + r + "</body>")
+def make_table():
+    print("""<meta charset='UTF-8'>
+    <style>
+    table {
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    td, th {
+        text-align: left;
+        padding: 8px;
+    }
+
+    tr:nth-child(even) {
+        background-color: #eeeeee;
+    }
+    </style>""")
+
+    with open(results_path, "r") as f:
+        data = json.load(f)
+
+    r = "<table>"
+    r += "<tr><td><b>Name</b></td><td><b>Board Size</b></td><td><b>Bombs</b></td><td><b>Result</b></td><td><b>Moves<b/></td></tr>"
+    for d in data:
+        r += "<tr><td>{0}</td><td>{1}</td><td>{2}</td><td>{3}</td><td>{4}</td></tr>".format(d["name"], d["board_size"], d["bombs"], d["result"], d["moves"])
+    r += "</table>"
+    print("<html><head><title>test.py</title></head><body><h1>Minesweeper!@@@</h1><p>Game results:</p>" + r + "</body>")
+
+
+def add_entry(file, entry):
+    with open(file, "r") as f:
+        data = json.load(f)
+    data.append(entry)
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def add_save(file, name, board):
+    with open(file, "r") as f:
+        data = json.load(f)
+    data[name] = board
+    with open(file, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def delete_all_saves(file):
+    with open(file, "w") as f:
+        json.dump({}, f, indent=2)
+
+
+def delete_entry(file, name):
+    with open(file, "r") as f:
+        data = json.load(f)
+    new_data = []
+    for entry in data:
+        if entry["name"] != name:
+            new_data.append(entry)
+    with open(file, "w") as f:
+        json.dump(new_data, f, indent=2)
+
+
+def delete_all_entries(file):
+    with open(file, "w") as f:
+        json.dump([], f, indent=2)
+
+
+def sort_results(col_name, reverse=False):
+    with open(results_path, "r") as f:
+        data = json.load(f)
+    data.sort(key=lambda x: x[col_name], reverse=reverse)
+    with open(results_path, "w") as f:
+        json.dump(data, f, indent=2)
+
+
+def main():
+    form = cgi.FieldStorage()
+
+    if "op" not in form.keys():
+        print("Missing op!")
+        return
+
+    if form["op"].value == "entry":
+        entry = {}
+        print(form.keys())
+        for k in form.keys():
+            if k == "op":
+                continue
+            value = form[k].value
+            entry[k] = int(value) if value.isdigit() and k in ("bombs", "moves") else value
+        print("Entry:", end="")
+        print(entry)
+        add_entry(results_path, entry)
+    elif form["op"].value == "save":
+        print("SAVE DATA!")
+        add_save(save_path, form["name"].value, json.loads(form["board"].value))
+    elif form["op"].value == "load":
+        print("LOAD DATA!")
+    elif form["op"].value == "delete":
+        print("DELETE DATA!")
+        delete_all_saves(save_path)
+    elif form["op"].value == "display":
+        print("DISPLAY DATA!")
+        sort_results("name")
+        make_table()
+    else:
+        print("Something went wrong! Bad form data!")
+
+
+if __name__ == '__main__':
+    main()
---
+++ public_html/prax3/app.js
@@ -0,0 +1,218 @@
+// todo: every number has a different color
+// todo: css touch ups
+
+
+$(document).ready(function () {
+    console.log("Document ready.");
+    handleBeginButton();
+    handleButtonPost($("#js-save"), {"op": "save", "name": "tempname", "board": JSON.stringify([[1, 1], [0, 1]])})
+    handleButtonPost($("#js-load"), {"op": "load", "name": "tempname"})
+    handleButtonPost($("#js-delete"), {"op": "delete"})
+});
+
+// global variables
+let safeCellsLeft;
+let defaultColor = "rgba(0, 0, 0, 0)";
+let displayColor = "rgb(200, 200, 200)";
+let openedColor = "lightgreen";
+let movesMade = 0;
+let name = "unnamed";
+let minesCount = 0;
+
+// handle clicking on Begin button
+function handleBeginButton() {
+    $("#js-begin").click(function () {
+        $(".js-table").remove();
+        $(".js-alert").empty();
+        movesMade = 0;
+        $('#minesweeper-board').append('<table class="js-table"></table>');
+        let boardSize = $("#board-size").val();
+        console.log("Board size:" + boardSize);
+        minesCount = $("#js-mines").val();
+        if (isNaN(minesCount) || minesCount <= 0 || minesCount > boardSize * boardSize - 1 || String(minesCount) !== String(parseInt(minesCount, 10))) {
+            alert("Invalid mine count!");
+            console.log("Invalid mine count!");
+        } else {
+            console.log("Valid mine count!");
+            setupBoard(boardSize, minesCount);
+        }
+        name = $("#js-name");
+        safeCellsLeft = boardSize * boardSize - minesCount;
+    });
+}
+
+// setup the board
+function setupBoard(boardSize, minesCount) {
+    console.log("Setting up the board...");
+    let cellsAmount = boardSize * boardSize;
+    let cells = new Array(cellsAmount).fill(0);
+    cells.fill(1, 0, minesCount);
+    shuffle(cells);
+
+    let tableElement = $(".js-table");
+    let board = [], counter = 0;
+    board.length = 0;  // clear the array
+    for (let row = 0; row < boardSize; row++) {
+        let rowCells = [];
+        let rowElement = $("<tr>");
+        tableElement.append(rowElement);
+        for (let col = 0; col < boardSize; col++) {
+            rowCells[col] = cells[counter++];
+            let cellElement = $("<td>");
+            cellElement.attr("data-row", row);
+            cellElement.attr("data-col", col);
+            rowElement.append(cellElement);
+        }
+        board[row] = rowCells;
+    }
+    console.log(board);
+    handleClicks(tableElement, board, boardSize);
+}
+
+// shuffle an array in place
+function shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Math.floor(Math.random() * counter--);
+        [array[counter], array[index]] = [array[index], array[counter]];
+    }
+}
+
+// handle clicks on table
+function handleClicks(tableElement, board, size) {
+    tableElement.on('click', 'td', function () {
+        if ($(this).attr("class") === "been-clicked") return;  // disable clicking on opened cell
+        let notificationElem = $("#notification");
+        notificationElem.empty();
+        let row = parseInt($(this).attr("data-row"));
+        let col = parseInt($(this).attr('data-col'));
+        $(this).addClass("been-clicked");
+        movesMade++;
+        if (board[row][col] === 1) {  // step on a bomb
+            $(this).text("💣");
+            let listElement = $("<li>");
+            listElement.text("Lost with " + movesMade.toString() + " moves!");
+            $("#result-data").append(listElement);
+            console.log("Name: " + name.val())
+            console.log("Board size: " + size.toString() + "x" + size.toString())
+            console.log("Bombs: " + minesCount.toString())
+            console.log("Result: " + "Lost")
+            console.log("Moves: " + movesMade.toString())
+            let result = {"name": name.val(), "board_size": size.toString() + "x" + size.toString(), "bombs": minesCount, "result": "Win", "moves": movesMade}
+            result["op"] = "entry";
+            postResultData(result)
+            console.log(result);
+
+            // reveal all bombs
+            for (let row = 0; row < size; row++) {
+                for (let col = 0; col < size; col++) {
+                    if (board[row][col] === 1) {
+                        $('td[data-row="' + row + '"]td[data-col="' + col + '"]').text("💣");
+                    }
+                }
+            }
+            $(".js-table").off();
+            alert("You have lost!");
+        } else {
+            let bombs = getNumberOfBombs(row, col, size, board);
+            if (bombs === 0) {  // no bombs nearby
+                let neighbours = getNeighbours(row, col, size);
+                for (let i = 0; i < neighbours.length; i++) {  // open neighbours
+                    let [r, c] = neighbours[i];
+                    let cell = $('td[data-row="' + r + '"]td[data-col="' + c + '"]');
+                    let nBombs = getNumberOfBombs(r, c, size, board);
+                    if (nBombs !== 0) {  // don't show zeros
+                        cell.text(getNumberOfBombs(r, c, size, board));
+                    }
+                    if (cell.css("background-color") === defaultColor) {
+                        cell.css("background-color", displayColor);
+                        safeCellsLeft--;
+                    }
+                }
+            } else {  // one or more bombs nearby
+                $(this).text(bombs);
+            }
+            if ($(this).css("background-color") !== displayColor) safeCellsLeft--;
+        }
+        $(this).css("background-color", openedColor);
+        if (safeCellsLeft === 0) {
+            let listElement = $("<li>");
+            listElement.text("Won with " + movesMade.toString() + " moves!");
+            $("#result-data").append(listElement);
+            $(".js-table").off();
+            alert("You have won!");
+
+            console.log("Name: " + name.val())
+            console.log("Board size: " + size.toString() + "x" + size.toString())
+            console.log("Bombs: " + minesCount.toString())
+            console.log("Result: " + "Won")
+            console.log("Moves: " + movesMade.toString())
+
+            let result = {"name": name.val(), "board_size": size.toString() + "x" + size.toString(), "bombs": minesCount, "result": "Win", "moves": movesMade}
+            result["op"] = "entry";
+            postResultData(result);
+            console.log(result);
+
+        }
+    });
+}
+
+// get a list of neighbours
+function getNeighbours(row, col, size) {
+    let neighbours = [];
+    for (let dRow = -1; dRow <= 1; dRow++) {
+        for (let dCol = -1; dCol <= 1; dCol++) {
+            if (dRow === 0 && dCol === 0) continue;
+            if ((row + dRow) >= 0 && (row + dRow < size) && (col + dCol) >= 0 && (col + dCol < size)) {
+                neighbours.push([row + dRow, col + dCol]);
+            }
+        }
+    }
+    return neighbours;
+}
+
+// get the number of bombs nearby
+function getNumberOfBombs(row, col, size, board) {
+    let neighbours = getNeighbours(row, col, size);
+    let count = 0;
+    for (let i = 0; i < neighbours.length; i++) {
+        let [r, c] = neighbours[i];
+        if (board[r][c] === 1) count++;
+    }
+    return count;
+}
+
+// post result data after game
+let postResultData = (function (result) {
+    $.ajax({
+        type: "POST",
+        url: "http://dijkstra.cs.ttu.ee/~rareba/cgi-bin/test.py",
+        data: result,
+        success: function (data) {
+            console.log('Submission was successful.');
+            console.log(data);
+        },
+        error: function (data) {
+            console.log('An error occurred.');
+            console.log(data);
+        },
+    });
+});
+
+function handleButtonPost(buttonElem, postData) {
+    buttonElem.click(function () {
+        $.ajax({
+            type: "POST",
+            url: "http://dijkstra.cs.ttu.ee/~rareba/cgi-bin/test.py",
+            data: postData,
+            success: function (data) {
+                console.log('Submission was successful.');
+                console.log(data);
+            },
+            error: function (data) {
+                console.log('An error occurred.');
+                console.log(data);
+            },
+        });
+    });
+}
